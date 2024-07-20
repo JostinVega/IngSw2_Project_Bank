c545375b53cd60088b85b579527295776bce6693
Gabriela Salazar <gabriela.salazar@epn.edu.ec>
2024-07-19 23:12:32 -0500

    Modificación del componente "home.component.ts"

--- FRONT/banco-politecnico/src/app/home/home.component.ts
+++ FRONT/banco-politecnico/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AutenticacionService } from '../services/autenticacion.service';
 import * as bcrypt from 'bcryptjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,7 @@ export class HomeComponent implements OnInit {
   homeForm: FormGroup;
   errorMessage: string = '';
 
-  constructor(private fb: FormBuilder, private authService: AutenticacionService, private router: Router) {
+  constructor(private fb: FormBuilder, private authService: AutenticacionService, private router: Router, private http: HttpClient) {
     this.homeForm = this.fb.group({
       usuario: ['', Validators.required],
       contrasena: ['', Validators.required]
@@ -27,6 +28,7 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit(): void {
+
     if (this.homeForm.valid) {
       this.authService.obtenerCredenciales(this.homeForm.value.usuario).subscribe(
         async (response: any) => {
@@ -35,8 +37,9 @@ export class HomeComponent implements OnInit {
             if (passwordMatches) {
               console.log('Navigating with usuario:', this.homeForm.value.usuario);
               this.router.navigate(['/inicio'], { queryParams: { usuario: this.homeForm.value.usuario } });
+              /*this.validateRecaptcha(); (se elimina lo que esta y se coloca eso) */
             } else {
-              this.errorMessage = 'Usuario o contraseña está incorrecto';
+              this.errorMessage = '¡Error! Contraseña incorrecta, escríbala nuevamente.';
             }
           } else {
             this.errorMessage = 'Error al obtener las credenciales';
@@ -49,12 +52,30 @@ export class HomeComponent implements OnInit {
       );
     } else {
       if (this.homeForm.get('usuario')?.invalid) {
-        this.errorMessage = 'Usuario requerido';
+        this.errorMessage = 'El campo "Usuario" es obligatorio.';
       } else if (this.homeForm.get('contrasena')?.invalid) {
-        this.errorMessage = 'Ingresar la contraseña';
+        this.errorMessage = 'El campo "Contraseña" es obligatorio.';
       } else {
-        this.errorMessage = 'Llenar los campos requeridos';
+        this.errorMessage = 'Por favor, complete todos los campos requeridos.';
       }
+    }
+  }
+
+  validateRecaptcha(): void {
+    const recaptchaResponse = (document.querySelector('.g-recaptcha-response') as HTMLInputElement).value;
+    if (recaptchaResponse) {
+      this.http.post('https://www.google.com/recaptcha/api/siteverify', {
+        secret: 'YOUR_SECRET_KEY', // Reemplaza YOUR_SECRET_KEY con tu clave secreta
+        response: recaptchaResponse
+      }).subscribe((recaptchaResult: any) => {
+        if (recaptchaResult.success) {
+          this.router.navigate(['/inicio'], { queryParams: { usuario: this.homeForm.value.usuario } });
+        } else {
+          this.errorMessage = 'Por favor, complete el reCAPTCHA.';
+        }
+      });
+    } else {
+      this.errorMessage = 'Por favor, complete el reCAPTCHA.';
     }
   }
 }
