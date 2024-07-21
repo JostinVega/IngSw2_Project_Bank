3abb10b6589752a741278622f01e22167f740d32
Vickiann Jiménez <vickiann.jimenez@gmail.com>
2024-07-21 00:48:49 -0500

    configuracion Verificacion de Codigo-Recuperar conttraseña

--- FRONT/banco-politecnico/src/app/verificar-codigo/verificar-codigo.component.ts
+++ FRONT/banco-politecnico/src/app/verificar-codigo/verificar-codigo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PasswordService } from '../services/password.service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verificar-codigo',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./verificar-codigo.component.css']
 })
 export class VerificarCodigoComponent {
+  codigo: string = '';
+  numero_identidad: string = ''; // Asegúrate de obtener este valor del usuario
+
+  constructor(private passwordService: PasswordService, private router: Router) { }
+
+  verificarCodigo() {
+    const enteredCode = this.codigo.split('').join('');
+    this.passwordService.verifySecurityCode(this.numero_identidad, enteredCode).subscribe(
+      response => {
+        if (response.message === 'Código de seguridad verificado correctamente.') {
+          this.router.navigate(['/siguiente-pagina']);
+        } else {
+          // Manejar intentos fallidos
+        }
+      },
+      error => {
+        // Manejar errores
+      }
+    );
+  }
 
 }
